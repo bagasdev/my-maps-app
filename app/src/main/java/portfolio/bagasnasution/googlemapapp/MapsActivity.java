747e25fbf81031396d115032b11dca79c852cf0a
bagasdev <bagasndeveloper@gmail.com>
2018-05-28 17:04:27 +0700

    second commit

--- app/src/main/java/portfolio/bagasnasution/googlemapapp/MapsActivity.java
+++ app/src/main/java/portfolio/bagasnasution/googlemapapp/MapsActivity.java
@@ -1,5 +1,9 @@
 package portfolio.bagasnasution.googlemapapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,7 +14,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback, OnCameraIdleListener, OnMapClickListener, OnMapLongClickListener {
 
     private GoogleMap mMap;
 
@@ -22,15 +31,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        getPermission();
+    }
+
+    private void getPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        1
+                );
+            } else {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        1
+                );
+            }
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng jegarta = new LatLng(-6.175302, 106.827139);
+        mMap.addMarker(new MarkerOptions().position(jegarta).title("Monumen Nasional"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(jegarta));
+
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraIdleListener(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-6.175302, 106.827139), 12));
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] != PackageManager.PERMISSION_DENIED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+
+//                        mMap.getMyLocation()
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onCameraIdle() {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mMap.clear();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title(null);
+
+        mMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
 }
